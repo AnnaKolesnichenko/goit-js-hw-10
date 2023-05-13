74c43e49638ef64e5526e71c3378d4d4105e2e1f
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-05-13 16:28:17 +0300

    added

---
+++ src/fetchCountries.js
@@ -0,0 +1,12 @@
+const BASE_URL = 'https://restcountries.com/v3.1';
+
+function fetchCountry(country) {
+    return fetch(`${BASE_URL}/name/${country}`)
+    .then(response => {
+        return response.json();
+    })
+}
+
+export default {fetchCountry};
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 import './css/styles.css';
+import importFetch from './fetchCountries';
+import debounce from 'debounce';
+import Notiflix from 'notiflix';
+
+
+const countryInput = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
 const DEBOUNCE_DELAY = 300;
+
+function onInputSearch(e) {
+    e.preventDefault();
+
+    const inputQuery = countryInput.value
+
+    if(inputQuery.length <= 2) {
+        Notiflix.Notify.info('Please narrow oyur search by entering more symbols');
+        return;
+    }
+    importFetch.fetchCountry(inputQuery)
+    .then(result => console.log(result))
+    .then(renderListOfCountries)
+    .catch(error => console.log(error));
+}
+
+
+function renderListOfCountries(country) {
+
+    console.log(`hello from ${country}`);
+    console.log(` ${population}`);
+}
+
+countryInput.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
+
+
+/*https://restcountries.com/v3.1/{service}?fields={field},{field},{field}
+https://restcountries.com/v3.1/all?fields=name,capital,currencies*/
+
+
+
+
+
+
+
