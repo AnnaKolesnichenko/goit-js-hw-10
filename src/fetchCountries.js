a72a3ed2d4f6cc065ebed4f50bfbba4291ab7da9
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-05-13 21:40:50 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,7 +3,7 @@ const BASE_URL = 'https://restcountries.com/v3.1';
 function fetchCountry(country) {
     return fetch(`${BASE_URL}/name/${country}`)
     .then(response => response.json())
-    .catch(error => console.log(error)) 
+     
 }
 
 export default {fetchCountry};
